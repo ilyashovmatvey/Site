791b0d951f36863770416ae217e40cef30cfbd7d
ilyashovmatvey <ilyashov.mstvey@gmail.com>
2026-04-28 17:57:04 +0200

    New commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -61,4 +62,4 @@ def confuse3():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
